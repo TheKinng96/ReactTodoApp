bb50eed873bad5ce200834b1f2799a8038af5a50
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-15 15:05:21 +0900

    done the basic form of the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('')
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setTodos('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <form>
+        <FormControl>
+          <InputLabel>What's in your mind?</InputLabel>
+          <Input value={input} onChange={(string) => setInput(string.target.value)} />
+        </FormControl>
+        <Button variant="contained" color="primary" disabled={!input} onClick={addTodo}>Add</Button>
+
+        <ul>
+          {todos.map(todo => (
+            <li>{todo}</li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
